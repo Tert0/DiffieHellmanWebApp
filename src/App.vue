ad6fef377cbb3cb99d57dbb9edb49e47bd23fe36
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-06-24 21:14:13 +0200

    Added Formulars and improved Public G generation

--- src/App.vue
+++ src/App.vue
@@ -34,15 +34,17 @@
       </tr>
 
       <tr>
-        <td>Alice Public Key: {{ alice_public }}</td>
+        <td>Alice Public Key: {{ public_g }}<sup>{{ alice_private }}</sup> mod {{ public_p }} = {{ alice_public }}</td>
         <td></td>
-        <td>Bob Public Key: {{ bob_public }}</td>
+        <td>Bob Public Key: {{ public_g }}<sup>{{ bob_private }}</sup> mod {{ public_p }} = {{ bob_public }}</td>
       </tr>
 
       <tr>
-        <td>Shared Secret: {{ alice_shared_secret }}</td>
+        <td>Shared Secret: {{ bob_public }}<sup>{{ alice_private }}</sup> mod {{ public_p }} = {{ alice_shared_secret }}
+        </td>
         <td></td>
-        <td>Shared Secret: {{ bob_shared_secret }}</td>
+        <td>Shared Secret: {{ alice_public }}<sup>{{ bob_private }}</sup> mod {{ public_p }} = {{ bob_shared_secret }}
+        </td>
       </tr>
     </table>
   </div>
@@ -63,7 +65,8 @@ export default Vue.extend({
   },
   methods: {
     generatePrivateKey(g: number): number {
-      return Math.floor(Math.random() * g);
+      let key = Math.floor(Math.random() * g);
+      return key != 0 ? key : 1;
     },
     generateG(): number {
       return this.findPrimitive(this.public_p);
@@ -78,7 +81,7 @@ export default Vue.extend({
       return Object.values(result.slice(min));
     },
     generateP(): number {
-      const primes: Array<number> = this.getPrimes(5, 200);
+      const primes: Array<number> = this.getPrimes(100, 200);
       return primes[Math.floor(Math.random() * primes.length + 1) - 1];
     },
     isPrime(n: number): boolean {
@@ -96,7 +99,7 @@ export default Vue.extend({
 
       return true;
     },
-    power(x: number, y: any, p: number): number {
+    power(x: number, y: number, p: number): number {
       let res = 1;
 
       x = x % p;
@@ -128,16 +131,16 @@ export default Vue.extend({
     },
     findPrimitive(n: number): number {
       let s = new Set();
-      if (this.isPrime(n) == false)
+      if (!this.isPrime(n))
         return -1;
       let phi = n - 1;
 
       this.findPrimefactors(s, phi);
 
-      for (let r = 2; r <= phi; r++) {
+      for (let r = Math.floor(phi/2); r > 2; r--) {
         let flag = false;
         for (let it of s) {
-
+          console.log(`R: ${r} Y: ${phi / Number(it)} N: ${n} Result: ${this.power(r, phi / Number(it), n)}`)
           if (this.power(r, phi / Number(it), n) == 1) {
             flag = true;
             break;
@@ -184,11 +187,18 @@ export default Vue.extend({
 }
 
 table {
+  margin-left: 12.5%;
+  width: 75%;
   border-collapse: collapse;
+
   td, th {
     border: 1px solid #eeeeee;
     text-align: left;
     padding: 8px;
   }
+}
+
+.same_secrets {
+  border: 1px solid #1f950d;
 }
 </style>
