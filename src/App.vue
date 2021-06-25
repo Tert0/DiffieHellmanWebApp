c94c57fe4a07880ed00f6e4ca7c9b8f3373d561d
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-06-25 06:41:56 +0200

    Fixed some Generator Bugs (and added some new), Added Color for the Table and Prettiered the Code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <table :class="{ same_secret: alice_shared_secret === bob_shared_secret}">
+    <table :class="{ same_secret: alice_shared_secret === bob_shared_secret, error: alice_shared_secret !== bob_shared_secret }">
       <tr>
         <th>Alice</th>
         <th>Public Numbers</th>
@@ -10,16 +10,18 @@
       <tr>
         <td>
           Alice Private Key: {{ alice_private }}
-          <br>
-          <button @click="alice_private = generatePrivateKey(public_p)">Generate</button>
+          <br />
+          <button @click="alice_private = generatePrivateKey(public_p)">
+            Generate
+          </button>
         </td>
-        <td>
-          G={{ public_g }}
-        </td>
+        <td>G={{ public_g }}</td>
         <td>
           Bob Private Key: {{ bob_private }}
-          <br>
-          <button @click="bob_private = generatePrivateKey(public_p)">Generate</button>
+          <br />
+          <button @click="bob_private = generatePrivateKey(public_p)">
+            Generate
+          </button>
         </td>
       </tr>
 
@@ -27,23 +29,40 @@
         <td></td>
         <td>
           p={{ public_p }}
-          <br>
-          <button @click="public_p = generateP();public_g = generateG()">Generate</button>
+          <br />
+          <button
+              @click="
+              public_p = generateP();
+              public_g = generateG();
+            "
+          >
+            Generate
+          </button>
         </td>
         <td></td>
       </tr>
 
       <tr>
-        <td>Alice Public Key: {{ public_g }}<sup>{{ alice_private }}</sup> mod {{ public_p }} = {{ alice_public }}</td>
+        <td>
+          Alice Public Key: {{ public_g }}<sup>{{ alice_private }}</sup> mod
+          {{ public_p }} = {{ alice_public }}
+        </td>
         <td></td>
-        <td>Bob Public Key: {{ public_g }}<sup>{{ bob_private }}</sup> mod {{ public_p }} = {{ bob_public }}</td>
+        <td>
+          Bob Public Key: {{ public_g }}<sup>{{ bob_private }}</sup> mod
+          {{ public_p }} = {{ bob_public }}
+        </td>
       </tr>
 
       <tr>
-        <td>Shared Secret: {{ bob_public }}<sup>{{ alice_private }}</sup> mod {{ public_p }} = {{ alice_shared_secret }}
+        <td>
+          Shared Secret: {{ bob_public }}<sup>{{ alice_private }}</sup> mod
+          {{ public_p }} = {{ alice_shared_secret }}
         </td>
         <td></td>
-        <td>Shared Secret: {{ alice_public }}<sup>{{ bob_private }}</sup> mod {{ public_p }} = {{ bob_shared_secret }}
+        <td>
+          Shared Secret: {{ alice_public }}<sup>{{ bob_private }}</sup> mod
+          {{ public_p }} = {{ bob_shared_secret }}
         </td>
       </tr>
     </table>
@@ -51,24 +70,24 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'App',
+  name: "App",
   data() {
     return {
       alice_private: 0,
       bob_private: 0,
       public_g: 5,
-      public_p: 23,
-    }
+      public_p: 23
+    };
   },
   methods: {
     generatePrivateKey(p: number): number {
-      return Math.floor(Math.random() * (p-1)) + 1;
+      return Math.floor(Math.random() * (p - 1)) + 1;
     },
     generateG(): number {
-      return this.findPrimitive(this.public_p);
+      return this.findPrimitiveRootModulo(this.public_p);
     },
     getPrimes(min: number, max: number): Array<number> {
       const result = Array(max + 1)
@@ -80,21 +99,17 @@ export default Vue.extend({
       return Object.values(result.slice(min));
     },
     generateP(): number {
-      const primes: Array<number> = this.getPrimes(50, 150);
+      const primes: Array<number> = this.getPrimes(5, 40);
       return primes[Math.floor(Math.random() * primes.length + 1) - 1];
     },
     isPrime(n: number): boolean {
-      if (n <= 1)
-        return false;
-      if (n <= 3)
-        return true;
+      if (n <= 1) return false;
+      if (n <= 3) return true;
 
-      if (n % 2 == 0 || n % 3 == 0)
-        return false;
+      if (n % 2 == 0 || n % 3 == 0) return false;
 
       for (let i = 5; i * i <= n; i = i + 6)
-        if (n % i == 0 || n % (i + 2) == 0)
-          return false;
+        if (n % i == 0 || n % (i + 2) == 0) return false;
 
       return true;
     },
@@ -104,72 +119,51 @@ export default Vue.extend({
       x = x % p;
 
       while (y > 0) {
-        if (y & 1)
-          res = (res * x) % p;
+        if (y & 1) res = (res * x) % p;
 
         y = y >> 1;
         x = (x * x) % p;
       }
       return res;
     },
-    findPrimefactors(s: Set<unknown>, n: number): void {
-      while (n % 2 == 0) {
-        s.add(2);
-        n = n / 2;
+    isPrimitiveRootModulo(g: number, n: number): boolean {
+      let found = new Set();
+      for (let i = 1; i < n * 100 /*hack - would need to detect cycles*/; i++) {
+        found.add(Math.pow(g, i) % n);
       }
-
-      for (let i = 3; i <= Math.sqrt(n); i = i + 2) {
-        while (n % i == 0) {
-          s.add(i);
-          n = n / i;
+      for (let i = 1; i < n; i++) {
+        if (!found.has(i)) {
+          return false;
         }
       }
-
-      if (n > 2)
-        s.add(n);
+      return true;
     },
-    findPrimitive(n: number): number {
-      let s = new Set();
-      if (!this.isPrime(n))
-        return -1;
-      let phi = n - 1;
-
-      this.findPrimefactors(s, phi);
-
-      for (let r = Math.floor(phi/2); r > 2; r--) {
-        let flag = false;
-        for (let it of s) {
-          console.log(`R: ${r} Y: ${phi / Number(it)} N: ${n} Result: ${this.power(r, phi / Number(it), n)}`)
-          if (this.power(r, phi / Number(it), n) == 1) {
-            flag = true;
-            break;
-          }
+    findPrimitiveRootModulo(n: number): number {
+      for (let i = 1; i < n; i++) {
+        if (this.isPrimitiveRootModulo(i, n)) {
+          return i;
         }
-
-        if (!flag)
-          return r;
       }
-
       return -1;
     }
   },
   mounted() {
-    this.public_p = this.generateP()
+    this.public_p = this.generateP();
     this.public_g = this.generateG();
     this.alice_private = this.generatePrivateKey(this.public_p);
     this.bob_private = this.generatePrivateKey(this.public_p);
   },
   computed: {
-    alice_public: function (): number {
+    alice_public: function(): number {
       return Math.pow(this.public_g, this.alice_private) % this.public_p;
     },
-    bob_public: function (): number {
+    bob_public: function(): number {
       return Math.pow(this.public_g, this.bob_private) % this.public_p;
     },
-    alice_shared_secret: function (): number {
+    alice_shared_secret: function(): number {
       return Math.pow(this.bob_public, this.alice_private) % this.public_p;
     },
-    bob_shared_secret: function (): number {
+    bob_shared_secret: function(): number {
       return Math.pow(this.alice_public, this.bob_private) % this.public_p;
     }
   }
@@ -190,7 +184,8 @@ table {
   width: 75%;
   border-collapse: collapse;
 
-  td, th {
+  td,
+  th {
     border: 1px solid #eeeeee;
     text-align: left;
     padding: 8px;
@@ -198,12 +193,21 @@ table {
 }
 
 .same_secret {
-  td, th {
-    border: 5px solid #1f950d;
+  border-collapse: collapse;
+  td,
+  th {
+    border: 2px solid #1f950d;
+  }
+}
+
+.error {
+  border-collapse: collapse;
+  td,
+  th {
+    border: 2px solid #a31010;
   }
 }
 
 button {
-
 }
 </style>
